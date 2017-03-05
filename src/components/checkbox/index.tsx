3ec184d78e37cbe12b8915a47f4fb8ca84b65cd6
AVVlasov <lizmatik@yandex.ru>
2017-03-05 18:13:44 +0300

    победил css

--- src/components/checkbox/index.tsx
+++ src/components/checkbox/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import * as DOM from 'react-dom';
-// import './index.css';
-// const Style = require<any>("./index.css");
+import './style.css'; // так заработало
+//const Style = require("./style.css"); // и так заработало 
 
 export class Checkbox extends React.Component<any, any> {
     constructor(props: any){
         super(props);
     }
     render() {
-        return <input type="checkbox" className="checkbox"/>;
+        return <div className="checkbox">
+            <input id="check1" type="checkbox" name="check" value="check1"/>
+            <label htmlFor="check1">Тест 1</label>
+            <br/>
+            <input id="check2" type="checkbox" name="check" value="check2"/>
+            <label htmlFor="check2">Тест 2</label>
+            </div>;
     }
 }
