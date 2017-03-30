90a539387f33cf893dd70afacb83bce8adbdc9a8
AVVlasov <lizmatik@yandex.ru>
2017-03-30 22:04:56 +0300

    fix export

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {checkbox} from "ui_mediapult";
+//import {Checkbox} from "./components/checkbox";
 import { Hello } from "./components/Hello";
-
+import {Checkboxq} from "./components/checkboxq";
+import {Checkbox, Newcheckbox} from "ui_mediapult";
 
 ReactDOM.render(
     <div>
-        <checkbox name="Name of checkbox!"/>
+        <Checkboxq name="checkbox first"/>
+        <Checkbox name="checkbox second2"/>
+        <Newcheckbox name="checkbox tree"/>
         <Hello programmer1="Pasha" programmer2="Vadim"/>
-        <Hello programmer1="Andrey" programmer2="Alexandr" />
+        <Hello programmer1="Andrey" programmer2="Alexandr" />      
     </div>,
     document.getElementById("example")
 );
