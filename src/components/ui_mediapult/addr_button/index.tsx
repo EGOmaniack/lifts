ae1e9244893a23fdc7c5136c767854c43d938c1d
EGOmaniack <EGOmaniack@mail.ru>
2017-04-02 08:17:28 +1000

    утро 02ого Не работает ссылка на pointer

--- src/components/ui_mediapult/addr_button/index.tsx
+++ src/components/ui_mediapult/addr_button/index.tsx
@@ -1,17 +1,34 @@
 import * as React from 'react';
 import * as DOM from 'react-dom';
 import styled from 'styled-components';
+//import { Row, Col } from 'react-bootstrap';
+//import { Button } from 'react-bootstrap';
 
 const Outer = styled.div`
    width: fit-content;
-
 `
 
-const BtnBody = styled.div`
+const Content = styled.div`
+    line-height: 36px;
+    vertical-align: middle;
+`
+const Text = styled.div`
+    line-height: 36px;
+    float: left;
+`
+const Icon = styled.div`
+    float: left;
     height: 11px;
     width: 8px;
+    background: palevioletred url(components/ui_mediapult/addr_button/img/pointer_inv.png) ;
+`
+const BtnBody = styled.div`
+    line-height: 36px;
+    height: 36px;
+    width: 100px;
+    border: 1px solid #2ea2f8;
+    border-radius: 3px;
     color: green;
-    background: palevioletred url("img/pointer_inv.png") ;
 `
 
 export class Addr_button extends React.Component<any, any> {
@@ -20,10 +37,12 @@ export class Addr_button extends React.Component<any, any> {
     }
     render() {
         return <Outer>
-                    <BtnBody className="wrap"></BtnBody>
+                    <BtnBody>
+                        <Content>
+                        <Icon></Icon>
+                        <Text>{this.props.value}</Text>
+                        </Content>
+                    </BtnBody>
                 </Outer>;
     }
 }
-
-//<input id="check1" type="checkbox" className="checkbox"/>
-//<label htmlFor="check1" >{this.props.name}</label>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import * as ReactDOM from "react-dom";
 //import {Checkbox} from "./components/checkbox";
 import { Hello } from "./components/Hello";
 import {Checkbox, Newcheckbox, Addr_button} from "./components/ui_mediapult";
+import * as txt from './constants/liftPackages';
 //test comment
+
+
+
 ReactDOM.render(
     <div>
         <Checkbox name="checkbox second2"/>
         <Newcheckbox name="checkbox tree"/>
         <Hello programmer1="Pasha" programmer2="Vadim"/>
         <Hello programmer1="Andrey" programmer2="Alexandr" />
-        <Addr_button active />      
+        <Addr_button value={txt.addr} active />      
     </div>,
     document.getElementById("example")
 );
